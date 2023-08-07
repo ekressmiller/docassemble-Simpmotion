ac41d6003c9d31d2a2d8483602f11acb4aa0a804
Emily K. Miller <110936328+ekressmiller@users.noreply.github.com>
2023-08-07 17:08:07 -0400

    Don't ask if multiple attorneys, if there's only one other party.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.Simpmotion',
-      version='1.0',
+      version='1.1',
       description=('Motion and Notice of Hearing'),
       long_description='# docassemble.Simpmotion\r\n\r\nMotion and Notice of Hearing\r\n\r\n## Author\r\n\r\nMichigan Legal Help\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -54,7 +54,7 @@ setup(name='docassemble.Simpmotion',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.22.1'],
+      install_requires=['docassemble.AssemblyLine>=2.24.0', 'docassemble.mlhframework'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/Simpmotion/', package='docassemble.Simpmotion'),
      )
