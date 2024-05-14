604247d543be356dabc0d5b4045b566fafa1293e
Emily Miller <110936328+ekressmiller@users.noreply.github.com>
2024-05-14 14:38:25 -0400

    basic method to collect data at start and end of interview

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.Simpmotion',
       url='https://michiganlegalhelp.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AssemblyLine>=2.27.2', 'docassemble.mlhframework'],
+      install_requires=['docassemble.AssemblyLine>=2.28.0', 'docassemble.mlhframework'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/Simpmotion/', package='docassemble.Simpmotion'),
      )
